172f27f4b6f8fbd2be1a0bf4e9520ad91e76f4d2
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-05-28 16:52:21 -0400

    finish hourly forecast

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
+import datetime
+
 from flask import Flask, render_template
+
+from weather_information import current_alerts, current_observations, hourly_forecast
 
 app = Flask(__name__)
 
@@ -7,7 +11,41 @@ app = Flask(__name__)
 def home():
     location = 39.14, -77.28
 
-    return render_template("index.html")
+    forecast, city_name, utc_offset = hourly_forecast(location)
+    cur_time = datetime.datetime.utcnow() - datetime.timedelta(hours=utc_offset * -1)
+    threshold = 6
+
+    new_fcst = []
+    start_idx = None
+
+    for idx, hour in enumerate(forecast):
+        if start_idx and idx >= start_idx + threshold:
+            break
+
+        if int(hour["time"].split(" ")[0]) == cur_time.hour:
+            start_idx = idx
+
+        new_fcst.append(hour)
+
+    cur_time_fmt = cur_time.strftime('%I:%M %p').lower().lstrip("0")
+
+    all_alerts = current_alerts(location)
+    icon, temp, wind_speed = current_observations(location, utc_offset)
+
+    return render_template(
+        "index.html",
+        date_fmt=cur_time_fmt,
+        city_name=city_name,
+        icon_url=icon,
+        temperature=temp,
+        wind_speed=wind_speed,
+        hour_1=new_fcst[0],
+        hour_2=new_fcst[1],
+        hour_3=new_fcst[2],
+        hour_4=new_fcst[3],
+        hour_5=new_fcst[4],
+        hour_6=new_fcst[5]
+    )
 
 
 @app.route("/login")
